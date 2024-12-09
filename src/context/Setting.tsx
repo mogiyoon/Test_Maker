444400f86de38ef4a3f313537e4193ad74f88f3c
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2024-12-09 22:05:52 +0900

    cache clean

--- src/context/Setting.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from "react"
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-const SettingContext = createContext({
-  name: '()',
-  mean: '[]',
-})
-
-export default SettingContext
