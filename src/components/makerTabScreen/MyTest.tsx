88889280887cb72450ca7dfc7d3f6340c9bbe9f6
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-01-13 01:52:42 +0900

    design: centered no data

--- src/components/makerTabScreen/MyTest.tsx
+++ src/components/makerTabScreen/MyTest.tsx
@@ -3,7 +3,16 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  width: 100%;
+  height: 100%;
+  padding: 2px;
 `;
+export const DataContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`
 export const StyledText = styled.Text`
   font-size: 10px;
 `;
