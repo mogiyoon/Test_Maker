f2327cd54d6155bc5f663e1048ceb27d26e3b25e
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-02-03 20:36:40 +0900

    license update

--- src/navigations/OpenSourceLicense.tsx
+++ src/navigations/OpenSourceLicense.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { Container, LicenseButton } from '../components/OpenSourceLicense'
 import { GridComponent } from '../components/GridComponent'
 import { openSourceLicenseJsonList } from '../../licenses/OpenSourceLicensesJson'
+import { TabNavigatorHomeIcon } from '../components/Tab';
+import { useNavigation } from '@react-navigation/native';
 
 export const OpenSourceLicense = () => {
+  const navigation = useNavigation()
+
   return (
     <Container>
+      <TabNavigatorHomeIcon
+        navigation={navigation}
+      />
       <GridComponent
         data={openSourceLicenseJsonList}
         renderItem={({item: inputItem}) => {
