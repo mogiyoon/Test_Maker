f2fb26286440a8e5b8c8029a90233805f77f7b3c
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-02-10 17:55:28 +0900

    fix: bug fix

--- src/assets/images/ImagesPath.tsx
+++
@@ -1,4 +0,0 @@
-export const loadingImages = {
-  loading1: require('./loading/loading1.png'),
-  loading2: require('./loading/loading2.png'),
-}
--- src/constants/Tutorials.tsx
+++
@@ -1,4 +0,0 @@
-export interface ExplainSet {
-  imageUri: string
-  explainText: string
-}
